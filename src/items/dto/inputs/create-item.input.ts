e66a841276062bc1bb9fbe2453df73c1bd4ea685
gustavo hernandez <hernandez29a@gmail.com>
2025-03-25 17:34:51 -0400

    clase 56

---
+++ src/items/dto/inputs/create-item.input.ts
@@ -0,0 +1,23 @@
+import { InputType, Int, Field, Float } from '@nestjs/graphql';
+import { IsNotEmpty, IsOptional, IsPositive, IsString, Min } from 'class-validator';
+
+@InputType()
+export class CreateItemInput {
+  //@Field(() => Int, { description: 'Example field (placeholder)' })
+  //exampleField: number;
+  
+  @Field( () => String )
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @Field( () => Float )
+  @IsPositive()
+  quantity: number;
+
+  @Field( () => String, { nullable: true } )
+  @IsString()
+  @IsOptional()
+  quantityUnits?: string;
+
+}
