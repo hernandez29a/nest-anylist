2d58d018ecb748115da97dc52ddcc143ded741dd
gustavo hernandez <hernandez29a@gmail.com>
2025-04-05 18:16:27 -0400

    cierre del modulo 7

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,46 @@
 import { ConfigModule } from '@nestjs/config';
 import { join } from 'path';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { ItemsModule } from './items/items.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+//import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { JwtService } from '@nestjs/jwt';
 
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    
+    //! Configuracion de GraphQL para obtener el login por medio de un resFul API
+   /*  GraphQLModule.forRootAsync({
+      driver: ApolloDriver,
+      imports: [ AuthModule ],
+      inject: [ JwtService],
+      useFactory: async ( jwtService: JwtService) => ({
+        playground: false,
+        autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+        plugins: [
+          ApolloServerPluginLandingPageLocalDefault(),
+        ],
+        context({req}){
+          const token  = req.headers.authorization?.replace('Bearer ', '');
+          if(!token) throw new Error('No token provided');
 
+          const payload = jwtService.decode(token);
+          if(!payload) throw new Error('Token not valid');          
+        } 
+        //debug: false,
+        //includeStacktraceInErrorResponses: false,
+      })
+    }), */
+
+
+    //* configuracion basica de graphql
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       // debug: false,
@@ -24,6 +51,8 @@ import { AuthModule } from './auth/auth.module';
       ],
       autoSchemaFile: join( process.cwd(), 'src/schema.gql'),
       }),
+
+
 
       TypeOrmModule.forRoot({
         type: 'postgres',
